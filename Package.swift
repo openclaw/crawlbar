941fa2b32c9ce79f6b88cb0caa2d2387a9ce1dbc
joshp123 <joshp123@users.noreply.github.com>
2026-06-10 22:59:01 +0200

    Raise CrawlBar package to macOS 26

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
     name: "CrawlBar",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v26),
     ],
     products: [
         .library(name: "CrawlBarCore", targets: ["CrawlBarCore"]),
